a4f8c3ed5d54ebcaa7841da2c262728a91a823c0
Henry Chu <henrytech@outlook.com>
2023-11-17 17:26:48 +0800

    test(elements): program counter

--- src/elements/instruction/pc.rs
+++ src/elements/instruction/pc.rs
@@ -7,16 +7,20 @@ pub struct ProgramCounter(u16);
 impl ProgramCounter {
     /// Creates a new program counter based on the given value with a given offset.
     pub fn offset(&self, offset: i32) -> Result<Self, InvalidOffset> {
-        u16::try_from(self.0 as i32 + offset)
+        offset
+            .checked_add(self.0 as i32)
+            .ok_or(InvalidOffset::I32(offset))
+            .map(|it| it as u16)
             .map(Self)
-            .map_err(|_| InvalidOffset::I32(offset))
     }
 
     /// Creates a new program counter based on the given value with a given offset (in [`i16`]).
     pub fn offset_i16(&self, offset: i16) -> Result<Self, InvalidOffset> {
-        u16::try_from(self.0 as i16 + offset)
+        offset
+            .checked_add(self.0 as i16)
+            .ok_or(InvalidOffset::I16(offset))
+            .map(|it| it as u16)
             .map(Self)
-            .map_err(|_| InvalidOffset::I16(offset))
     }
 }
 
@@ -50,4 +54,36 @@ pub enum InvalidOffset {
     I16(i16),
     #[error("Invalid i32 offset {0}")]
     I32(i32),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_offset() {
+        let pc = ProgramCounter::from(10);
+        assert_eq!(pc.offset(5).unwrap(), ProgramCounter::from(15));
+        assert_eq!(pc.offset(-5).unwrap(), ProgramCounter::from(5));
+        assert!(pc.offset(i32::MAX).is_err());
+    }
+
+    #[test]
+    fn test_offset_i16() {
+        let pc = ProgramCounter::from(10);
+        assert_eq!(pc.offset_i16(5).unwrap(), ProgramCounter::from(15));
+        assert_eq!(pc.offset_i16(-5).unwrap(), ProgramCounter::from(5));
+        assert!(pc.offset_i16(i16::MAX).is_err());
+    }
+
+    #[test]
+    fn test_default() {
+        assert_eq!(ProgramCounter::default(), ProgramCounter::from(0));
+    }
+
+    #[test]
+    fn test_display() {
+        let pc = ProgramCounter::from(10);
+        assert_eq!(format!("{}", pc), "#00010");
+    }
 }
