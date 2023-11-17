93c8003ae53133a241fdebcb0d452eb82de08e2d
Henry Chu <henrytech@outlook.com>
2023-11-17 17:12:08 +0800

    test(utils): merge

--- src/utils.rs
+++ src/utils.rs
@@ -17,3 +17,47 @@ where
         (lhs, rhs) => Ok(lhs.or(rhs)),
     }
 }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn merge_success() {
+        let lhs = Some(5);
+        let rhs = Some(10);
+        let result = merge(lhs, rhs, |a, b| a + b);
+        assert_eq!(result, Some(15));
+    }
+
+    #[test]
+    fn merge_with_none() {
+        let lhs = Some(5);
+        let rhs = None;
+        let result = merge(lhs, rhs, |a, b| a + b);
+        assert_eq!(result, Some(5));
+    }
+
+    #[test]
+    fn try_merge_success() {
+        let lhs = Some(5);
+        let rhs = Some(10);
+        let result = try_merge(lhs, rhs, |a, b| Ok::<_, ()>(a + b));
+        assert_eq!(result, Ok(Some(15)));
+    }
+
+    #[test]
+    fn try_merge_with_none() {
+        let lhs = Some(5);
+        let rhs = None;
+        let result = try_merge(lhs, rhs, |a, b| Ok::<_, ()>(a + b));
+        assert_eq!(result, Ok(Some(5)));
+    }
+
+    #[test]
+    fn try_merge_with_error() {
+        let lhs = Some(5);
+        let rhs = Some(10);
+        let result = try_merge(lhs, rhs, |_, _| Err("Error"));
+        assert_eq!(result, Err("Error"));
+    }
+}
