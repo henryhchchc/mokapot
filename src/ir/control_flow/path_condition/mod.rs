636b00947dfbf4e5dd10fafd3c56cb2f99912635
Henry Chu <henrytech@outlook.com>
2025-03-23 12:14:04 +0800

    fix: Add quine-mccluskey dependency and reimplement path condition simplification

--- src/ir/control_flow/path_condition/mod.rs
+++ src/ir/control_flow/path_condition/mod.rs
@@ -1,5 +1,9 @@
 //! Path constraint analysis.
-use std::{collections::BTreeSet, fmt::Display};
+use std::{
+    collections::{BTreeMap, BTreeSet, btree_set},
+    fmt::Display,
+    ops::{BitAnd, BitOr, Not},
+};
 
 use itertools::Itertools;
 
@@ -9,55 +13,81 @@ pub use analyzer::*;
 
 /// Path condition in disjunctive normal form.
 #[derive(Debug, PartialEq, Eq, Clone, PartialOrd, Ord)]
-pub struct PathCondition<P> {
+pub struct SOP<V> {
     /// The clauses in the disjunctive normal form.
     /// An empry set represents a contradiction.
     /// An singleton of an empty set represents a tautology.
-    products: BTreeSet<Conjunction<P>>,
+    minterms: BTreeSet<MinTerm<V>>,
 }
 
 /// A conjunction of predicates.
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub struct Conjunction<P>(BTreeSet<P>);
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct MinTerm<V>(BTreeSet<Variable<V>>);
 
-impl<P: Ord> FromIterator<P> for Conjunction<P> {
-    fn from_iter<T: IntoIterator<Item = P>>(iter: T) -> Self {
-        Self(BTreeSet::from_iter(iter))
+/// A variable in a path condition.
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub enum Variable<ID> {
+    /// A positive variable.
+    Positive(ID),
+    /// A negative variable.
+    Negative(ID),
+}
+
+impl<ID> Variable<ID> {
+    /// Returns a reference to the inner id of the variable.
+    pub const fn id(&self) -> &ID {
+        match self {
+            Variable::Negative(id) | Variable::Positive(id) => id,
+        }
     }
 }
 
-impl<P> std::ops::BitAnd for Conjunction<P>
+impl<V> Display for Variable<V>
+where
+    V: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Variable::Positive(id) => write!(f, "({id})"),
+            Variable::Negative(id) => write!(f, "~({id})"),
+        }
+    }
+}
+
+impl<V> Not for Variable<V> {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        match self {
+            Variable::Positive(id) => Variable::Negative(id),
+            Variable::Negative(id) => Variable::Positive(id),
+        }
+    }
+}
+
+impl<P> BitAnd for MinTerm<P>
 where
     P: Ord,
 {
     type Output = Self;
 
     fn bitand(self, rhs: Self) -> Self::Output {
-        let Conjunction(mut new) = self;
-        let Conjunction(rhs) = rhs;
+        let MinTerm(mut new) = self;
+        let MinTerm(rhs) = rhs;
         new.extend(rhs);
-        Conjunction(new)
+        MinTerm(new)
     }
 }
 
-impl<P> Conjunction<P> {
-    /// Creates a conjunction of predicates.
+impl<P> MinTerm<P> {
+    /// Creates a tautology (i.e., ⊤).
     #[must_use]
-    pub fn from_predicates(predicates: impl IntoIterator<Item = P>) -> Self
-    where
-        P: Ord,
-    {
-        Self(BTreeSet::from_iter(predicates))
-    }
-
-    /// Creates a tautology.
-    #[must_use]
-    pub fn tautology() -> Self {
+    pub fn one() -> Self {
         Self(BTreeSet::default())
     }
 }
 
-impl<P: Display> Display for Conjunction<P> {
+impl<P: Display> Display for MinTerm<P> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         if self.0.is_empty() {
             write!(f, "⊤")
@@ -67,151 +97,162 @@ impl<P: Display> Display for Conjunction<P> {
     }
 }
 
-impl<P> IntoIterator for Conjunction<P> {
-    type Item = P;
-    type IntoIter = std::collections::btree_set::IntoIter<P>;
+impl<V: Ord> FromIterator<Variable<V>> for MinTerm<V> {
+    fn from_iter<T: IntoIterator<Item = Variable<V>>>(iter: T) -> Self {
+        let inner = iter.into_iter().collect();
+        MinTerm(inner)
+    }
+}
+
+impl<V> IntoIterator for MinTerm<V> {
+    type Item = Variable<V>;
+    type IntoIter = btree_set::IntoIter<Variable<V>>;
 
     fn into_iter(self) -> Self::IntoIter {
         self.0.into_iter()
     }
 }
 
-impl<P> PartialOrd for Conjunction<P>
-where
-    P: PartialOrd,
-{
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        match self.0.len().partial_cmp(&other.0.len()) {
-            Some(std::cmp::Ordering::Equal) => self.0.partial_cmp(&other.0),
-            it => it,
-        }
+impl<'a, V> IntoIterator for &'a MinTerm<V> {
+    type Item = &'a Variable<V>;
+    type IntoIter = btree_set::Iter<'a, Variable<V>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.iter()
     }
 }
 
-impl<P> Ord for Conjunction<P>
-where
-    P: Ord,
-{
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        match self.0.len().cmp(&other.0.len()) {
-            std::cmp::Ordering::Equal => self.0.cmp(&other.0),
-            it => it,
-        }
-    }
-}
-
-impl<P> PathCondition<P> {
+impl<P> SOP<P> {
     /// Creates a tautology.
     #[must_use]
-    pub fn tautology() -> Self
+    pub fn one() -> Self
     where
         P: Ord,
     {
-        let products = BTreeSet::from([Conjunction::tautology()]);
-        Self { products }
+        let products = BTreeSet::from([MinTerm::one()]);
+        Self { minterms: products }
     }
 
     /// Creates a contradiction.
     #[must_use]
-    pub fn contradiction() -> Self
+    pub fn zero() -> Self
     where
         P: Ord,
     {
         let products = BTreeSet::default();
-        Self { products }
-    }
-
-    /// Creates a conjunction of predicates.
-    #[must_use]
-    pub fn conjunction_of(predicates: impl IntoIterator<Item = P>) -> Self
-    where
-        P: Ord,
-    {
-        let product = Conjunction::from_predicates(predicates);
-        let products = BTreeSet::from([product]);
-        Self { products }
+        Self { minterms: products }
     }
 
     /// Simplifies the path condition.
     pub fn simplify(&mut self)
     where
-        P: Ord + Clone + std::ops::Not<Output = P>,
+        P: Ord + Clone,
     {
-        // FIXME: The current implementation is buggy. See Issue #10.
-        loop {
-            let mut any_removal = false;
-            // Apply absorption laws.
-            // i.e. Aa + A!ab = Aa + Ab
-            let pairs_of_products: Vec<_> = self
-                .products
-                .iter()
-                .flat_map(|lhs| self.products.iter().map(move |rhs| (lhs, rhs)))
-                .collect();
-
-            let new_products: BTreeSet<_> = pairs_of_products
-                .into_iter()
-                .filter_map(|(lhs, rhs)| {
-                    if let Some((single,)) = lhs.0.difference(&rhs.0).collect_tuple() {
-                        let mut rhs_diff = rhs.0.difference(&lhs.0).collect::<BTreeSet<_>>();
-                        if rhs_diff.contains(&!single.clone()) {
-                            rhs_diff.remove(&!single.clone());
-                            let factor: BTreeSet<_> = lhs.0.intersection(&rhs.0).collect();
-                            let new_rhs = rhs_diff.union(&factor).map(|it| (*it).clone()).collect();
-                            return Some(new_rhs);
-                        }
-                    }
-                    None
-                })
-                .collect();
-            // Adding simplified new products will lead to removal in the next iteration.
-            any_removal |= !new_products.is_empty();
-            self.products.extend(new_products);
-
-            // Remove redundant products.
-            let to_remove: BTreeSet<_> = self
-                .products
-                .iter()
-                .filter(|product| {
-                    // If a product is a super set of another product, the product is redundant.
-                    self.products.iter().any(|another_product| {
-                        product.0.len() > another_product.0.len()
-                            && product.0.is_superset(&another_product.0)
-                    })
-                })
-                .cloned()
-                .collect();
-            any_removal |= !to_remove.is_empty();
-            self.products.retain(|product| !to_remove.contains(product));
-
-            if !any_removal {
-                break;
-            }
+        use quine_mccluskey as qmc;
+        let variable_ids = self.variable_ids();
+        let idx_count = variable_ids.len();
+        if variable_ids.len() > qmc::DEFAULT_VARIABLES.len() {
+            todo!("Too many variables")
         }
+        let var_idx: BTreeMap<_, _> = variable_ids
+            .iter()
+            .enumerate()
+            .map(|(idx, var)| (*var, idx as u32))
+            .collect();
+        let var_map: BTreeMap<_, _> = qmc::DEFAULT_VARIABLES
+            .into_iter()
+            .zip(variable_ids)
+            .collect();
+        let minterms: Vec<_> = self.minterms.iter().map(|it| it.index(&var_idx)).collect();
+        let mut solutions = qmc::minimize_minterms(
+            &qmc::DEFAULT_VARIABLES[..idx_count],
+            &minterms,
+            &[],
+            true,
+            None,
+        )
+        .expect("There should be a result as no timeout is set.");
+        let qmc::Solution::SOP(sop_solution) = solutions
+            .pop()
+            .expect("There should be at least one solution")
+        else {
+            unreachable!("We are using minimize_minterms")
+        };
+        let new_sop = sop_solution
+            .into_iter()
+            .map(|min_terms| {
+                min_terms
+                    .into_iter()
+                    .map(|var| {
+                        let id = (*var_map.get(var.name.as_str()).unwrap()).clone();
+                        if var.is_negated {
+                            Variable::Negative(id)
+                        } else {
+                            Variable::Positive(id)
+                        }
+                    })
+                    .collect()
+            })
+            .collect();
+
+        *self = new_sop;
+    }
+
+    fn variable_ids(&self) -> BTreeSet<&P>
+    where
+        P: Ord,
+    {
+        self.minterms
+            .iter()
+            .flat_map(|term| term.into_iter())
+            .map(Variable::id)
+            .collect()
     }
 }
 
-impl<T> std::ops::BitOr for PathCondition<T>
+impl<V: Ord> MinTerm<V> {
+    fn index(&self, variable_map: &BTreeMap<&V, u32>) -> u32 {
+        let mut result = 0u32;
+        for var in self {
+            let id = var.id();
+            let idx = variable_map[id];
+            if let Variable::Positive(_) = var {
+                result |= 1 << idx;
+            }
+        }
+        result
+    }
+}
+
+impl<V: Ord> FromIterator<MinTerm<V>> for SOP<V> {
+    fn from_iter<T: IntoIterator<Item = MinTerm<V>>>(iter: T) -> Self {
+        let minterms = iter.into_iter().collect();
+        Self { minterms }
+    }
+}
+
+impl<T> BitOr for SOP<T>
 where
-    T: Ord + Clone + std::ops::Not<Output = T>,
+    T: Ord + Clone,
 {
     type Output = Self;
 
     fn bitor(self, rhs: Self) -> Self::Output {
-        let mut products = self.products;
-        products.extend(rhs.products);
-        PathCondition { products }
+        let mut products = self.minterms;
+        products.extend(rhs.minterms);
+        SOP { minterms: products }
     }
 }
 
-impl<T> std::ops::BitAnd for PathCondition<T>
+impl<V> BitAnd for SOP<V>
 where
-    T: Ord + Clone + std::ops::Not<Output = T>,
+    V: Ord + Clone,
 {
     type Output = Self;
 
     fn bitand(self, rhs: Self) -> Self::Output {
-        let PathCondition { products: this } = self;
-        let PathCondition { products: other } = rhs;
+        let SOP { minterms: this } = self;
+        let SOP { minterms: other } = rhs;
         let products = this
             .into_iter()
             .flat_map(|lhs_prod| {
@@ -221,16 +262,16 @@ where
                     .map(move |rhs_prod| lhs_prod.clone() & rhs_prod)
             })
             .collect();
-        PathCondition { products }
+        SOP { minterms: products }
     }
 }
 
-impl<T> Display for PathCondition<T>
+impl<T> Display for SOP<T>
 where
     T: Display,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let PathCondition { products } = self;
+        let SOP { minterms: products } = self;
         if products.is_empty() {
             write!(f, "⊥")
         } else {
@@ -246,45 +287,49 @@ mod test {
     use proptest::collection::btree_set;
     use proptest::prelude::*;
 
-    use crate::ir::control_flow::path_condition::Conjunction;
+    use crate::ir::control_flow::path_condition::MinTerm;
 
-    use super::PathCondition;
+    use super::{SOP, Variable};
 
-    #[derive(Debug, PartialEq, Eq, Clone, PartialOrd, Ord, proptest_derive::Arbitrary)]
-    struct TestPredicate(pub u32, pub bool);
+    impl proptest::arbitrary::Arbitrary for Variable<u32> {
+        type Parameters = (u32, bool);
+        type Strategy = Just<Self>;
 
-    impl std::ops::Not for TestPredicate {
-        type Output = Self;
-
-        fn not(self) -> Self::Output {
-            Self(self.0, !self.1)
+        fn arbitrary_with(args: Self::Parameters) -> Self::Strategy {
+            let (id, negative) = args;
+            if negative {
+                Just(Variable::Negative(id))
+            } else {
+                Just(Variable::Positive(id))
+            }
         }
     }
 
-    fn evaluate(cond: PathCondition<TestPredicate>, value_map: &HashMap<u32, bool>) -> bool {
-        cond.products
+    fn evaluate(cond: SOP<u32>, value_map: &HashMap<u32, bool>) -> bool {
+        cond.minterms
             .into_iter()
-            .map(|product| product.into_iter().all(|it| value_map[&it.0] == it.1))
+            .map(|product| product.into_iter().all(|it| value_map[it.id()]))
             .reduce(|lhs, rhs| lhs || rhs)
             .unwrap_or_default()
     }
 
-    fn generate_pred_values(cond: &PathCondition<TestPredicate>) -> HashMap<u32, bool> {
-        cond.products
+    fn generate_pred_values(cond: &SOP<u32>) -> HashMap<u32, bool> {
+        cond.minterms
             .iter()
             .flat_map(|it| it.0.iter())
-            .map(|it| it.0)
+            .map(Variable::id)
+            .copied()
             .dedup()
             .map(|it| (it, rand::random()))
             .collect()
     }
 
-    fn arb_test_cond() -> impl Strategy<Value = PathCondition<TestPredicate>> {
+    fn arb_test_cond() -> impl Strategy<Value = SOP<u32>> {
         btree_set(
-            btree_set(any::<TestPredicate>(), 1..10).prop_map(Conjunction),
+            btree_set(any::<Variable<u32>>(), 1..20).prop_map(MinTerm),
             1..10,
         )
-        .prop_map(|products| PathCondition { products })
+        .prop_map(|products| SOP { minterms: products })
     }
 
     proptest! {
